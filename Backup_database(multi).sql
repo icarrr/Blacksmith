2834aa653339d642f15cf03db17b1048d1f0d1a5
icarrr <faizarsangster@gmail.com>
2019-09-13 10:37:28 +0700

    [Deleted] Remove spesific backup path

--- Backup_database(multi).sql
+++ Backup_database(multi).sql
@@ -3,8 +3,6 @@ DECLARE @path VARCHAR(256) -- path for backup files
 DECLARE @fileName VARCHAR(256) -- filename for backup  
 DECLARE @fileDate VARCHAR(20) -- used for file name 
 
-SET @path = 'C:\Program Files\Microsoft SQL Server\MSSQL12.SQLEXPRESS\MSSQL\Backup\'  
-
 SELECT @fileDate = REPLACE(CONVERT(VARCHAR(8), GETDATE(),112),'/','') + '_' +  REPLACE(CONVERT(VARCHAR, GETDATE(),108),':','')
 
 DECLARE db_cursor CURSOR FOR  
@@ -17,7 +15,7 @@ FETCH NEXT FROM db_cursor INTO @name
 
 WHILE @@FETCH_STATUS = 0   
 BEGIN   
-       SET @fileName = @path + @name + '_' + @fileDate + '.bak'  
+       SET @fileName = @name + '_' + @fileDate + '.bak'  
        BACKUP DATABASE @name TO DISK = @fileName WITH NOFORMAT, NOINIT,  NAME = N'Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10;
 
        FETCH NEXT FROM db_cursor INTO @name   
